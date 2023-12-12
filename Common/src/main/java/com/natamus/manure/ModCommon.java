a26bb800b2c6161be823c5b6f97ad2e057c241fd
Rick South <rick@natamus.com>
2023-12-12 09:44:45 +0100

    Manure source code for 1.20.3.

---
+++ Common/src/main/java/com/natamus/manure/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.manure;
+
+import com.natamus.manure.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+
+	}
+}
